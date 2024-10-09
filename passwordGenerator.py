bbeec3b663df8c7a5baad74cac956fa1a350e57b
Rafael Cerutti <magnuslima123@gmail.com>
2024-10-09 12:38:38 -0300

    Adição de escolha para encerrar programa

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,12 +1,22 @@
 import string
 import random
 
-character = string.ascii_uppercase + string.ascii_letters + string.digits
-password = ''
+def passwordGenerator():
+    character = string.ascii_uppercase + string.ascii_letters + string.digits
+    password = ''
 
-size = int(input('How many characters will your password have?: '))
+    size = int(input('How many characters will your password have?: '))
+    for i in range(size):
+        password += random.choice(character)
 
-for i in range(size):
-    password += random.choice(character)
+    return password
 
-print(f'Your passaword is: {password}')
+while True:
+    password = passwordGenerator()
+    print(password)
+    choice = int(input('Enter 1 for a new password or enter 0 to finish: '))
+
+    if choice == 0:
+        print('Program finished')
+        break
+
